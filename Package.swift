b187f7d259d2e5f49560b7f85405935460e7990c
Peter Pan <apppeterpan@gmail.com>
2021-08-03 10:20:38 +0800

    update dependencies

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@ import PackageDescription
 let package = Package(
 	name: "FolioReaderKit",
 	dependencies: [
-		.Package(url: "https://github.com/ZipArchive/ZipArchive.git", majorVersion: 2, minor: 1),
-		.Package(url: "https://github.com/cxa/MenuItemKit.git", majorVersion: 3, minor: 0),
-		.Package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", majorVersion: 0, minor: 6),
-		.Package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4, minor: 2),
-		.Package(url: "https://github.com/ArtSabintsev/FontBlaster.git", majorVersion: 4, minor: 0),
-		.Package(url: "https://github.com/fantim/JSQWebViewController.git", majorVersion: 6, minor: 1),
-		.Package(url: "https://github.com/realm/realm-cocoa.git", majorVersion: 3, minor: 1),
+		.package(url: "https://github.com/ZipArchive/ZipArchive.git", .exact("2.1")),
+		.package(url: "https://github.com/cxa/MenuItemKit.git", .exact("3.0")),
+		.package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", .exact("0.6")),
+		.package(url: "https://github.com/tadija/AEXML.git", .exact("4.2")),
+		.package(url: "https://github.com/ArtSabintsev/FontBlaster.git", .exact("4.0")),
+		.package(url: "https://github.com/fantim/JSQWebViewController.git", .exact("6.1")),
+		.package(url: "https://github.com/realm/realm-cocoa.git", .exact("3.1")),
 	]
 )
