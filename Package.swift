f93110c850364ad20b2bb517ed40df7f29b5c486
Peter Pan <apppeterpan@gmail.com>
2021-08-03 10:37:56 +0800

    update

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/ZipArchive/ZipArchive.git", .exact("2.1.0")),
 		.package(url: "https://github.com/cxa/MenuItemKit.git", .exact("3.0.0")),
-		.package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", from: "0.6"),
+		.package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", .revision("163314622f39bdc8aeb275979766be89cab7dfcc")),
 		.package(url: "https://github.com/tadija/AEXML.git", .exact("4.2.0")),
 		.package(url: "https://github.com/ArtSabintsev/FontBlaster.git", .exact("4.0.0")),
 		.package(url: "https://github.com/fantim/JSQWebViewController.git", .exact("6.1.1")),
